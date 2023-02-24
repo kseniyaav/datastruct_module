0ac4883a13eef421790d1e20c4a880049db1ef7b
kseniyaav <kseniyaav10@gmail.com>
2023-02-24 20:34:03 +0300

    - class added, .gitignore added

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+import os
+from googleapiclient.discovery import build
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from dotenv import load_dotenv
+api_key = 'AIzaSyBf8KYKumDUiXf2SE0d1VTndB41creSfDw'
 
+class Youtube:
+    channel_id = "UCByhZ-JEe5OOZSuq0uaXOng"
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    def __init__(self, channel_id, channel_name, channel_description, hm_followers, hm_videos, hm_views):
+        load_dotenv()
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        self.channel_id = channel_id
+        self.channel_name = channel_name
+        self.channel_description = channel_description
+        self.hw_followers = hm_followers
+        self.hw_videos = hm_videos
+        self.hm_views = hm_views
+        self.channel_info = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
+    def print_info(self):
+        return self.channel_info
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if __name__ == "__main__":
+    y = Youtube(1, 2, 3, 4, 5, 6)
+    print(y.print_info())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
