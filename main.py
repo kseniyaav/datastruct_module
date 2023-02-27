efb11d9ae85d051fffe637a9df2ad9964a3659fb
kseniyaav <kseniyaav10@gmail.com>
2023-02-27 13:50:04 +0300

    - @staticmethod, @property, def to_json added

--- main.py
+++ main.py
@@ -1,27 +1,64 @@
 import os
+import json
+
 from googleapiclient.discovery import build
-
 from dotenv import load_dotenv
-api_key = 'AIzaSyBf8KYKumDUiXf2SE0d1VTndB41creSfDw'
 
-class Youtube:
-    channel_id = "UCByhZ-JEe5OOZSuq0uaXOng"
+load_dotenv()
 
-    def __init__(self, channel_id, channel_name, channel_description, hm_followers, hm_videos, hm_views):
-        load_dotenv()
+api_key = os.getenv('API_KEY')
+
+class Channel:
+    def __init__(self, channel_id_arg):
+        youtube = self.get_service()
+        channel_info = youtube.channels().list(
+            id=channel_id_arg, part='snippet,statistics'
+        ).execute()
+        snippet = channel_info['items'][0]['snippet']
+        statistics = channel_info['items'][0]['statistics']
+
+        self._channel_id = channel_id_arg
+        self.title = snippet['title']
+        self.description = snippet['description']
+        self.url = f'https://www.youtube.com/channel/{channel_id_arg}'
+        self.subscriber_count = int(statistics['subscriberCount'])
+        self.video_count = int(statistics['videoCount'])
+        self.view_count = int(statistics['viewCount'])
+
+    @staticmethod
+    def get_service():
         youtube = build('youtube', 'v3', developerKey=api_key)
-        self.channel_id = channel_id
-        self.channel_name = channel_name
-        self.channel_description = channel_description
-        self.hw_followers = hm_followers
-        self.hw_videos = hm_videos
-        self.hm_views = hm_views
-        self.channel_info = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        return youtube
+
+    @property
+    def channel_id(self):
+        return self._channel_id
+
+    @channel_id.setter
+    def channel_id(self, new_channel_id):
+        self._channel_id = new_channel_id
+        self.url = f'https://www.youtube.com/channel/{new_channel_id}'
 
     def print_info(self):
         return self.channel_info
 
+    def to_json(self, filename):
+        data = {
+            'channel_id': self._channel_id,
+            'title': self.title,
+            'description': self.description,
+            'url': self.url,
+            'subscriber_count': self.subscriber_count,
+            'video_count': self.video_count,
+            'view_count': self.view_count
+        }
+        with open(filename, 'w') as f:
+            json.dump(data, f)
+
 if __name__ == "__main__":
-    y = Youtube(1, 2, 3, 4, 5, 6)
-    print(y.print_info())
+    vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
+
+
+
+
 
